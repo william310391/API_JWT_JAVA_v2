0c2ffe2d448c57f753a9480f51dd728078523661
William <william310391@gmail.com>
2023-07-30 23:23:15 -0500

    implementacion de entidades de seguridad

--- src/main/java/JWTAPI/Repository/SeguridadRepository.java
+++ src/main/java/JWTAPI/Repository/SeguridadRepository.java
@@ -3,9 +3,11 @@ package JWTAPI.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import JWTAPI.Entity.Seguridad;
 
+@Repository
 public interface SeguridadRepository extends JpaRepository<Seguridad, Integer> {
     @Query(value = "select * from seguridad where usuario=:usuario", nativeQuery = true)
     Seguridad findSeguridadByUsuario(@Param("usuario") String usuario);
